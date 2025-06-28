c377be2a2511b5a21d66100bf75f09d401eae895
asqarbek <warsanasa777@gmail.com>
2025-06-28 14:56:49 +0100

    register boldi

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 from .settings import MEDIA_URL, MEDIA_ROOT
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/v1/auth/", include("authenticate.urls")),
-    path("api/v1/app", include("app.urls"))
+    path("", include("authenticate.urls")),
+    path("", include("app.urls")),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ] +  static(MEDIA_URL, document_root=MEDIA_ROOT)
